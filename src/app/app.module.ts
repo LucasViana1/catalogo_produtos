590a8454b2ea5f6ee09ef1dce041dce90d91b287
Lucas Cunha <lucas.cunha@GRUPOIV2.LOCAL>
2020-02-27 12:38:16 -0300

    estrutura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PoModule } from '@portinari/portinari-ui';
+import { RouterModule } from '@angular/router';
+import { CategoryListComponent } from './pages/Category/category-list/category-list.component';
+import { HomeComponent } from './pages/Home/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PoModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
